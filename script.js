4cb68147a1222e9c7baf72be86790e6fa2f670bd
Paulo Samuel <paulosamuelr2024@icloud.com>
2026-06-29 16:45:51 -0300

    tentando enviar as informações para o dc

--- script.js
+++ script.js
@@ -1,3 +1,62 @@
+const axios = require("axios");
+const WEBHOOK_URL = "https://discord.com/api/webhooks/1521236604029501491/ZAa08P7z47LaYbF7f8BltNujj9a27-W_dJ5pIKYXHoLpklRqeZdeExl5qw_t_gRRGqHz";
+
+const participanteTexto = participantes.join("\n• ");
+const comandosTexto = comandos.join("\n• ");
+
+await axios.post(WEBHOOK_URL, {
+    embeds: 
+    [
+        {
+        title: "📋 Nova Operação Registrada",
+        color: 3447003,
+        fields: [
+            {
+                name: "👤 QRA",
+                value: qra,
+                inline: true
+            },
+            {
+                name: "📅 Data",
+                value: data_operacao,
+                inline: true
+            },
+            {
+                name: "🎯 QRU",
+                value: qru,
+                inline: false
+            },
+            {
+                name: "🏴 Facção",
+                value: faccao,
+                inline: true
+            },
+            {
+                name: "🏆 Resultado",
+                value: resultado,
+                inline: true
+            },
+            {
+                name: "👥 Participantes",
+                value: participantesTexto || "-",
+                inline: false
+            },
+            {
+                name: "⭐ Comando",
+                value: comandosTexto || "-",
+                inline: false
+            },
+            {
+                name: "📝 Observação",
+                value: observacao || "Nenhuma",
+                inline: false
+            }
+        ],
+        timestamp: new Date()
+        }
+    ]
+});
+
 const participantes = document.querySelectorAll(
     '.checkbox-group input[type="checkbox"]'
 );
